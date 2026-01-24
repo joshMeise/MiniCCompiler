17bae9e3df2e324c801dc1ade919c2237498ad96
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2026-01-24 10:31:02 -0500

    execs deleted

--- execs/compiler.c
+++ execs/compiler.c
@@ -13,3 +13,29 @@
 extern int yyparse(void);
 extern int yylex_destroy(void);
 extern FILE* yyin;
+
+int main(int argc, char** argv) {
+    int ret;
+
+    // Check arguments.
+    if (argc != 2) {
+        fprintf(stderr, "No program provided.\n");
+        return 1;
+    }
+
+    // Open file.
+    if ((yyin = fopen(argv[1], "r")) == NULL) {
+        fprintf(stderr, "Failed to open file.\n");
+        return 1;
+    }
+
+    ret = yyparse();
+
+    // Clean up.
+    fclose(yyin);
+    yylex_destroy();
+
+
+    if (ret == 0) exit(EXIT_SUCCESS);
+    else exit(EXIT_FAILURE);
+}
