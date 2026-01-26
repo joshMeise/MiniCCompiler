fdae5a3b7f932b580119f60c1b38d984ab5d074b
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2026-01-26 01:38:27 +0000

    push for work on mac

--- execs/prog.c
+++ execs/prog.c
@@ -4,5 +4,21 @@ extern int read(void);
 int main(void) {
     int a;
     a = -1;
+
+    if (a < 4) 
+        return 1;
+    else
+        return 4;
+
+    if (a > 4) {
+        int i;
+
+        i = 5;
+    } else {
+        int i;
+        i = 5;
+        print(i);
+    }
+
     return 0;
 }
---
+++ utils/semantic_analysis.h
@@ -0,0 +1,11 @@
+/*
+ * semantic_analysis.h - 
+ *
+ * Josh Meise
+ * 01-25-2026
+ * Description: 
+ *
+ */
+
+#pragma once
+#include "ast.h"
