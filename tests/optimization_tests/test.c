9a5e3d0e4a0c800bcd82178b3a1d30cf037cfe87
Josh Meise <joshua.m.meise.24@dartmouth.edu>
2026-03-04 20:41:59 +0000

    more assembly gen

--- tests/optimization_tests/test.c
+++ tests/optimization_tests/test.c
@@ -4,16 +4,21 @@ extern int read();
 int func(int p){
 	int a;
 	int b;
-	a = 0;
+	int c;
+    int d;
+    int e;
 
-	if (p < 0)
-		a = 10;
-	else {
-		int a;
-		a = 2;
-		b = 0;
-		while (b < p) 
-			b = b + a;
-	}
-	return a+b;
+    a = 4 + p;
+
+    b = a + p;
+
+    c = a - 3;
+
+    d = a - 3;
+
+    e = b + a;
+
+    a = b + e;
+
+	return a + b;
 }
--- utils/assembly_generator.cpp
+++ utils/assembly_generator.cpp
@@ -11,6 +11,8 @@
 #include <unordered_set>
 #include "assembly_generator.h"
 
+#define NUM_REGS 3
+
 static LLVMValueRef get_last_use(LLVMBasicBlockRef bb, LLVMValueRef inst) {
     LLVMValueRef last_inst;
     LLVMUseRef use, last_use;
@@ -27,17 +29,22 @@ static LLVMValueRef get_last_use(LLVMBasicBlockRef bb, LLVMValueRef inst) {
     }
 
     // Walk through all instructions in basic block.
+    last_use = NULL;
     while (use != NULL && LLVMGetInstructionParent(LLVMGetUser(use)) == bb) {
         last_use = use;
         use = LLVMGetNextUse(use);
     }
 
-    if ((last_inst = LLVMGetUser(last_use)) == NULL) {
-        std::cerr << "No instruction user found.\n";
-        return NULL;
+    // Return instruction itself if no uses.
+    if (last_use != NULL) {
+        if ((last_inst = LLVMGetUser(last_use)) == NULL) {
+            std::cerr << "No instruction user found.\n";
+            return NULL;
+        }
+        return last_inst;
+    } else {
+        return inst;
     }
-
-    return last_inst;
 }
 
 static std::optional<std::unordered_map<LLVMValueRef, int>> get_inst_index(LLVMBasicBlockRef bb) {
@@ -77,7 +84,7 @@ static std::optional<std::unordered_map<LLVMValueRef, std::pair<int, int>>> get_
     // Get live range for each relevant instruction.
     for (i = LLVMGetFirstInstruction(bb); i != NULL; i = LLVMGetNextInstruction(i)) {
         // Only deal with instructions which have a left hand side.
-        if (LLVMGetTypeKind(LLVMTypeOf(i)) != LLVMVoidTypeKind) {
+        if (LLVMGetInstructionOpcode(i) != LLVMAlloca && LLVMGetTypeKind(LLVMTypeOf(i)) != LLVMVoidTypeKind) {
             if ((last_use = get_last_use(bb, i)) == NULL) {
                 std::cerr << "Failed to find last use.\n";
                 return std::nullopt;
@@ -92,12 +99,15 @@ static std::optional<std::unordered_map<LLVMValueRef, std::pair<int, int>>> get_
 
 std::optional<std::unordered_map<LLVMValueRef, int>> allocate_registers(LLVMModuleRef m) {
     LLVMBasicBlockRef bb;
+    LLVMValueRef inst, operand;
+    LLVMOpcode opcode;
     std::unordered_set<int> avail_regs;
     std::unordered_map<LLVMValueRef, int> reg_map;
     std::unordered_map<LLVMValueRef, int> inst_index;
     std::optional<std::unordered_map<LLVMValueRef, int>> inst_index_opt;
     std::optional<std::unordered_map<LLVMValueRef, std::pair<int, int>>> live_range_opt;
     std::unordered_map<LLVMValueRef, std::pair<int, int>> live_range;
+    int i;
 
     // Check argument.
     if (m == NULL) {
@@ -124,6 +134,42 @@ std::optional<std::unordered_map<LLVMValueRef, int>> allocate_registers(LLVMModu
             return std::nullopt;
         } else
             live_range = live_range_opt.value();
+
+        // Initialize set of available registers.
+        for (i = 0; i < NUM_REGS; i++)
+            avail_regs.insert(i);
+
+        // Loop through all instructions in basic block.
+        for (inst = LLVMGetFirstInstruction(bb); inst != NULL; inst = LLVMGetNextInstruction(inst)) {
+            // Instructions that do not have a result.
+            if (LLVMGetTypeKind(LLVMTypeOf(inst)) == LLVMVoidTypeKind) {
+
+                // Check all operands and see if live range ends.
+                for (i = 0; i < LLVMGetNumOperands(inst); i++) {
+                    operand = LLVMGetOperand(inst, i);
+
+                    // Ignore alloca instructions.
+                    if (LLVMGetInstructionOpcode(operand) != LLVMAlloca && !LLVMIsAArgument(operand)) {
+                        // Check if live range ends.
+                        if (live_range[operand].second == inst_index[inst]) {
+                            // If operand is currently assigned a register, add the register to the set of available registers.
+                            if (reg_map[operand] != -1)
+                                avail_regs.insert(reg_map[operand]);
+                        }
+                    }
+                }
+            }
+            // Instructions that do have a result (ignoring alloca instructions).
+            else if (LLVMGetInstructionOpcode(inst) != LLVMAlloca && LLVMGetTypeKind(LLVMTypeOf(inst)) != LLVMVoidTypeKind) {
+                opcode = LLVMGetInstructionOpcode(inst);
+                // Instructions with two operands.
+                // Question: In the algorithm it specifies onluym add, mul and sub but should we be considering lt, gt, etc too?
+                if (opcode == LLVMAdd || opcode == LLVMSub || opcode == LLVMMul) {
+
+                }
+
+            }
+        }
     }
 
     return reg_map;
